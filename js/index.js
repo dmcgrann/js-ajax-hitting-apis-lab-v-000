aa4f89ba31953f6cf8c51ae1c12117c8266b565e
David McGrann <dpmcgrann@gmail.com>
2019-02-02 18:31:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,9 +12,7 @@ function displayRepositories() {
       r =>
         '<li>' +
         r.name +
-        ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+        '</li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
