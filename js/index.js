28e6aa1d880eeca64b0c8b65509e11c000cbb351
David McGrann <dpmcgrann@gmail.com>
2019-02-02 18:37:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -37,6 +37,6 @@ function getBranches(el) {
 
 function displayBranches() {
   let branches = JSON.parse(this.responseText);
-  const branchesList = `<ul>${branches.map(b => '<li>' + b.branch + '</li>').join('')}</ul>`;
+  const branchesList = `<ul>${branches.map(b => '<li>' + b.branches_url + '</li>').join('')}</ul>`;
   document.getElementById('details').innerHTML = branchesList;
 }
