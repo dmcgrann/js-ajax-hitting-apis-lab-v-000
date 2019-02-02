74bff654509acdf04571ddcc7d32025fe2680a83
David McGrann <dpmcgrann@gmail.com>
2019-02-02 18:30:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function getRepositories() {
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos
     .map(
