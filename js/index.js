25e6dc5957e0992340cda813da2d1500afae617b
David McGrann <dpmcgrann@gmail.com>
2019-02-02 18:27:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ function showRepositories() {
 
 function getCommits(el) {
   const name = el.dataset.username;
-  const name = el.dataset.repo;
+  const repo = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
